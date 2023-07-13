18d3f9339001ebb0f9a559376fda252f342b1902
Taasin Alvi <77426735+RobinDoughnut@users.noreply.github.com>
2023-07-13 22:09:20 +0600

    Initial Commit

---
+++ Bumbelbee/BumbelbeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumbelbeeApp.swift
+//  Bumbelbee
+//
+//  Created by Taasin Bin Hossain Alvi on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BumbelbeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bumbelbee/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bumbelbee
+//
+//  Created by Taasin Bin Hossain Alvi on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
